bb927b4c01dd651baed8dd104def7552253ddcdf
C-Bretton <Christian-bretton@hotmail.com>
2024-04-11 10:37:57 +0100

    Co-authored-by: ChristianBretton <C-Bretton@users.noreply.github.com>

--- Example/cep2app/Cep2Main.py
+++ Example/cep2app/Cep2Main.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     # Create a data model and add a list of known Zigbee devices.
     devices_model = Cep2Model()
     devices_model.add([Cep2ZigbeeDevice("0x00158d000572a560", "pir"),
-		       Cep2ZigbeeDevice("0x00158d000836d221", "pir"),
+                       Cep2ZigbeeDevice("0x00158d000836d221", "pir"),
                        Cep2ZigbeeDevice("0x680ae2fffec0cbba", "led"),
                        Cep2ZigbeeDevice("0x680ae2fffe72463b", "power plug")])
 
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 
 if __name__ == "__main__":
-    print("Go check on you stove!!")
+    print("Go check on your stove!!")
 
 
